eebce01a345439735873b831013f6fdaefa480e8
poojithaPoojaj <poojitha6789012@gmail.com>
2023-05-04 17:21:35 +0530

    Pages Implementation ===================== 1. Implemented Search and GetSearchItems list 2. Implemented navigateTo method in LauncherPage

--- src/main/java/framework/ultralesson/webautomation/pages/HomePage.java
+++ src/main/java/framework/ultralesson/webautomation/pages/HomePage.java
@@ -1,19 +1,40 @@
 package framework.ultralesson.webautomation.pages;
 
 import framework.ultralesson.webautomation.models.Item;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomePage {
+
+    WebDriver webDriver;
+    By searchBar=By.id("twotabsearchtextbox");
+
+    By searchResults=By.cssSelector("div.s-suggestion-container");
+    By searchResultsContainer=By.className("left-pane-results-container");
+    By recentSearches=By.className("s-recentSearchDistinct");
     public HomePage(WebDriver driver) {
+        this.webDriver=driver;
     }
-    public void search(String searchItem){
+    public HomePage search(String searchItem){
+        webDriver.findElement(searchBar).click();
+        webDriver.findElement(searchBar).sendKeys(searchItem);
+        return this;
 
     }
 
     public List<Item> getSearchItems() {
-        return new ArrayList<>();
+        List<WebElement> elements =webDriver.findElements(recentSearches);
+        List<Item> items = new ArrayList<>();
+        for(WebElement element : elements) {
+            String name = element.getText();
+            Item item = new Item();
+            item.setName(name);
+            items.add(item);
+        }
+        return items;
     }
 }
